bdec1de5a7d4ba821bf2c0018da3c8844f215571
danb31 <daniel.bell96@hotmail.co.uk>
2024-05-07 13:18:56 +0100

    new files

---
+++ synthetic_population/clean_datasets.py
@@ -0,0 +1,119 @@
+import glob
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def load_df(path):
+    print("loading csv to dataframe: ", path)
+    files = glob.iglob(os.path.join(path, "*.csv"))
+    temp = []
+    for filename in files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        temp.append(df)
+    return pd.concat(temp, axis=0, ignore_index=True)
+
+
+def remove_outliers(df):
+    residential_type_list = [-2, 26]
+    if 'QS420_CELL' in df.columns:
+        return df.loc[df['QS420_CELL'].isin(residential_type_list)]
+    elif 'QS420_CELL_x' in df.columns:
+        return df.loc[df['QS420_CELL_x'].isin(residential_type_list)]
+
+
+def export_to_csv(df, path):
+    print("exporting to csv: ", path)
+    df.to_csv(path, encoding='utf-8', header=True)
+
+
+def main(persons_input_dir = os.getenv("persons_input_dir"),
+         households_input_dir = os.getenv("households_input_dir"),
+         MSOA_OA_input_dir = os.getenv("MSOA_OA_input_path"),
+         persons_output_path = os.getenv("persons_cleaned_path"),
+         households_output_path = os.getenv("households_cleaned_path")):
+
+    df_persons_NE = load_df(persons_input_dir)
+    df_households_NE = load_df(households_input_dir)
+    df_MSOA_OA_2011_NE_list = pd.read_csv(MSOA_OA_input_dir, index_col=None,
+                                          header=0)
+
+    print("No.of.unique person values :", len(pd.unique(df_persons_NE['Area'])))
+    print("No.of.unique household values :",
+          len(pd.unique(df_households_NE['Area'])))
+
+    # Modify columns
+    df_persons_NE = df_persons_NE.rename(
+        {'Area': 'Area_MSOA', 'DC1117EW_C_SEX': 'Sex',
+         'DC1117EW_C_AGE': 'Age', 'DC2101EW_C_ETHPUK11': 'Ethnic'}, axis=1)
+    df_households_NE = df_households_NE.rename({'Area': 'Area_OA'}, axis=1)
+    df_households_NE = df_households_NE.astype({"HID": int})
+
+    # Create a new column in 'df_persons_NE' for the unique ID --> PID_Area_MSOA
+    df_persons_NE["PID_AreaMSOA"] = df_persons_NE["PID"].astype(str) + '_' + \
+                                    df_persons_NE["Area_MSOA"]
+    df_households_NE["HID_AreaOA"] = df_households_NE["HID"].astype(str) + '_' \
+                                     + df_households_NE["Area_OA"]
+
+    # Join df_households_NE with df_MSOA_OA_2011_NE_list in order to get MSOA
+    # level within df_households_NE and then pass the Area_OA to df_persons_NE
+    # based on the MSOA level value
+    df_households_NE = df_households_NE.merge(df_MSOA_OA_2011_NE_list,
+                                              left_on='Area_OA', right_on='oa',
+                                              how='left')
+
+    # Rename the column names of 'df_persons_NE'
+    df_households_NE = df_households_NE.rename({'msoa': 'Area_MSOA'}, axis=1)
+
+    # Merge df_persons_NE with df_households_NE in order to get the Area_OA for
+    # each person
+    df_persons_NE = pd.merge(df_persons_NE, df_households_NE, how='left',
+                             left_on=['HID', 'Area_MSOA'],
+                             right_on=['HID', 'Area_MSOA'])
+
+    # Create a new column in df_households_NE combining HRPID with Area_MSOA in
+    # order to then match the Household refernce person (HRPID) from the
+    # household dataframe with the person in the df_persons_NE
+    df_households_NE["HRPID_AreaMSOA"] = df_households_NE["HRPID"].astype(
+        str) + '_' + df_households_NE["Area_MSOA"]
+
+    # Merge df_persons_NE with df_households_NE AGAIN in order to get the
+    # HRPID_Area_MSOA for each person. The goal with this is that the new
+    # LC4605_C_NSSEC will be only assigned to the household reference person.
+    # The rest of member of the household will have an empty value in this
+    # column
+    df_persons_NE = pd.merge(df_persons_NE, df_households_NE, how='left',
+                             left_on=['PID_AreaMSOA'],
+                             right_on=['HRPID_AreaMSOA'])
+
+    # KEEP ONLY the required columns and rename
+    df_persons_NE = df_persons_NE[
+        ['PID_AreaMSOA', 'PID', 'Area_MSOA_x', 'Area_OA_x', 'Sex', 'Age',
+         'Ethnic', 'HID_x', 'HID_AreaOA_x', 'LC4408_C_AHTHUK11_x',
+         'LC4404_C_SIZHUK11_x', 'LC4605_C_NSSEC_x', 'LC4202_C_CARSNO_x',
+         'LC4202_C_ETHHUK11_x', 'HRPID_AreaMSOA', 'LC4605_C_NSSEC_y',
+         'QS420_CELL_x']]
+    df_persons_NE = df_persons_NE.rename({'LC4605_C_NSSEC_y': 'NSSEC'}, axis=1)
+
+    # Remove rows from df_persons_NE which HIP = (-1), as they are not assigned
+    # to any household. They should be considered as leftovers in the process of
+    # matching persons with households
+    # TODO: Changed 'HID' to 'HID_x' as there is no 'HID' column after selecting
+    # TODO: required columns in persons. Is this correct?
+    df_persons_NE = df_persons_NE[df_persons_NE['HID_x'] != -1]
+
+    # Remove those PEOPLE and HOUSEHOLDS that do not belong to residential
+    # households (QS420_CELL = -2) or to student accommodations
+    # (QS420_CELL = 26)
+    df_persons_NE = remove_outliers(df_persons_NE)
+    df_households_NE_extended = remove_outliers(df_households_NE)
+
+    # Export dataframes into csv files
+    export_to_csv(df_persons_NE, persons_output_path)
+    # TODO: Should this df be 'df_households_NE' or 'df_households_NE_extended'?
+    export_to_csv(df_households_NE_extended, households_output_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ synthetic_population/economic_activity.py
@@ -0,0 +1,313 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def generate_inactive_people(df):
+    # Concatenate a new dataframe containing only those people which NSSEC value
+    # is null or equal to 9 (students)
+    return pd.concat([df.loc[(df['NSSEC'].isnull())],
+                      df.loc[(df['NSSEC'] == 9)]])
+
+
+def process_OA_area(OA_area, df_economic_activity, df_composition, gender,
+                    age_range, inactive_conversor):
+    # Select the row of the df_economic_activity that is related to the selected
+    # OA area:
+    df_economic_activity_area = df_economic_activity.loc[
+        (df_economic_activity['geography'] == OA_area)]
+
+    # Select ALL people that live in the selected OA area and are >= 16 years old:
+    df_people_OA_area = df_composition.loc[(df_composition['Area_OA_x'] == OA_area)
+                                           & (df_composition['Age'] >= 16)]
+
+    # Select only those that belong to a specific gender (male of female) and
+    # age range. The INACTIVE people will be chosen from this dataset. The
+    # number of them will depend on the value of variable "globals()[f"inactive_
+    # {gender}_{age_range[0]}_{age_range[1]}_2019"]"
+    df = df_people_OA_area.loc[
+        (df_people_OA_area['Sex'] == gender)
+        & (df_people_OA_area['Age'] >= age_range[0])
+        & (df_people_OA_area['Age'] <= age_range[1])
+    ]
+
+    # Calculate the total number of people with the specific gender and range of
+    # age living in that OA area:
+    total_in_OA_2019 = len(df.index)
+
+    # Create a variable per type of economic activity based on sex and range of
+    # age
+    filter_cols = gender + "_" + str(age_range[0]) + "_" + str(age_range[1])
+
+    # Identify the number of people based on sex and range of age in the
+    # selected OA area that are employed
+    # Value from 2011!!
+    # TODO: Value comming table LC6107EW that has to be updated to 2019
+    employment_status = ["Employed", "Unemployed", "Inactive"]
+    employment_dict = {}
+    for status in employment_status:
+        employment_dict[status] = df_economic_activity_area.iloc[
+            0, df_economic_activity_area.columns.get_loc(filter_cols+"_"+status)
+        ]
+
+    # Value with the total number of people living in the seleted OA area (table
+    # LC6107EW)
+    total = sum(employment_dict.values())
+
+    # Ratio of people 2019 vs 2011:
+    # If the number of people of the selected gender and range of age is greater
+    # than 0 (e.g., there are people in the OA with this specific sex and range
+    # of age). If value > 1, it means there are more people in 2019 in the
+    # selected OA area of the specific gender and range of age. If there are no
+    # people (gender and age range) in the OA area, then the ratio will be equal
+    # to 1.
+    if total > 0:
+        ratio_people_2019_2011 = total_in_OA_2019 / total
+    else:
+        ratio_people_2019_2011 = 1
+
+    # New value for 2019 = (Value from 2011 (table LC6107EW)) * (inactive
+    # conversor value (based on age range and sex)) * (ratio of people 2019 vs
+    # 2011 (c)). This value is the number of people that will be randomly
+    # assigned "inactive" based on their OA area, range of age and sex.
+    inactive_2019 = int(round(employment_dict["Inactive"] * inactive_conversor
+                              * ratio_people_2019_2011, 0))
+
+    # SELECT FIRST STUDENTS
+    df_students = df.loc[(df['LC4605_C_NSSEC_x'] == 9)]
+
+    # If there are STUDENTS in the OA area:
+    # TODO: Refactored logic - test
+    if not df_students.empty:
+        # Select randomly the number of people that will be assigned as inactive
+        num_inactive = min(inactive_2019, len(df_students))
+        df_inactive_students = df_students.sample(num_inactive)
+    else:
+        df_inactive_students = pd.DataFrame()
+
+    # Remaining NUMBER of people to be assigned as INACTIVE:
+    remaining_inactive_2019 = inactive_2019 - len(df_inactive_students)
+
+    # Force remaining people to be INACTIVE where NSSEC value is null or 9, and
+    # remove previously selected students in df_inactive_students.
+
+    # Dataframe containing people of the selected OA area, sex, range of age and
+    # nssec null.
+    # TODO: What are these dataframes?
+    df_potential_inactive_NSCCnull = df_potential_inactive.loc[
+        (df_potential_inactive['Area_OA_x'] == OA_area)
+        & (df_potential_inactive['Sex'] == gender)
+        & (df_potential_inactive['Age'] >= age_range[0])
+        & (df_potential_inactive['Age'] <= age_range[1])
+        & (df_potential_inactive['NSSEC'].isnull())]
+
+    # Dataframe containing people of the selected OA area, sex, range of age and
+    # nssec 9 (student)
+    df_potential_inactive_NSCC9 = df_potential_inactive.loc[
+        (df_potential_inactive['Area_OA_x'] == OA_area)
+        & (df_potential_inactive['Sex'] == gender)
+        & (df_potential_inactive['Age'] >= age_range[0])
+        & (df_potential_inactive['Age'] <= age_range[1])
+        & (df_potential_inactive['NSSEC'] == 9)]
+
+    # This dataframe contains the people of the selected OA area, sex, range of
+    # age and nssec (null or 9) that will be used to select the remaining
+    # INACTIVE people. But before that, the previous selected students
+    # df_inactive_students MUST be removed.
+    df_potential_inactive = pd.concat([
+        df_potential_inactive_NSCCnull, df_potential_inactive_NSCC9])
+
+    # Remaining PEOPLE in the dataframe (df_gender_age0_age1 - (number of
+    # students already selected)):
+    # Concatenate previous selected students with the "potential inactives" and
+    # remove duplicates
+    df_potential_inactive_plus_students = pd.concat([
+        df_potential_inactive, df_inactive_students])
+    df_potential_inactive_remaining = df_potential_inactive_plus_students.drop_duplicates(
+        keep=False)
+
+    # Now, we are ready to select the remaining INACTIVE people:
+    # Select randomly the number of people to be inactive based on age and sex:
+    # TODO: Refactored logic - test
+    num_remaining_inactive = min(remaining_inactive_2019, len(df_potential_inactive_remaining))
+    df_inactive = df_potential_inactive_remaining.sample(
+        num_remaining_inactive) if num_remaining_inactive > 0 else pd.DataFrame()
+
+    # If there are still some people to be assigned as INACTIVE, then their
+    # NSSEC can be any value.
+    # Remaining NUMBER of people to be assigned as INACTIVE:
+    second_remaining_inactive_2019 = (inactive_2019 - len(df_inactive_students)
+                                      - len(df_inactive))
+
+    # Concatenate all people of the specific sex, range age, OA area with the
+    # selected students and the others wich NSSEC value is null or 9
+    concat_list = [df, df_inactive_students, df_inactive]
+    df_potential_inactive_plus_students_plus_null = pd.concat(concat_list)
+
+    # Remove duplicates
+    df_potential_inactive_remaining_last = df_potential_inactive_plus_students_plus_null.drop_duplicates(
+        keep=False)
+
+    # Select randomly the number of people to be inactive based on age and sex:
+    # TODO: Refactored logic - test
+    length_df = len(df_potential_inactive_remaining_last)
+    if length_df > 0:
+        sample_size = min(second_remaining_inactive_2019, length_df)
+        df_inactive_last = df_potential_inactive_remaining_last.sample(
+            sample_size)
+    else:
+        df_inactive_last = pd.DataFrame()
+
+    # Concatenate all persons "inactive" in one dataframe
+    df_inactive_all_each_area = pd.concat([df_inactive_students, df_inactive,
+                                           df_inactive_last])
+
+    return df_inactive_all_each_area
+
+
+def inactive_iterator(iteration_counter, total_inactive_percentage,
+                      AreaOA_list, df_economic_activity, df_composition, gender,
+                      age_range, persons_inactive):
+    # let's start assigning economic activities to the persons in the
+    # synthetic population based on the OA level, age and sex
+    # TODO: I don't understand this code - where is 'globals()[f"inactiv
+    #  e_rate_{gender}_{age_range[0]}_{age_range[1]}_2019"]' [changed to
+    #  'inactive_rate_2019'] assigned anywhere else, either in this
+    #  script or another? How is it able to do math operations?
+    while abs(inactive_rate_2019 - total_inactive_percentage) > 1:
+        iteration_counter += 1
+
+        print("It: ", iteration_counter, 'CONVERSOR Value: ',
+              inactive_conversor)
+
+        # Assign / Clear the dataframe everytime there is a need for a
+        # new iteration.
+        df_gender_inactive = pd.DataFrame()
+        df_inactive_students = pd.DataFrame()
+        df_inactive_last = pd.DataFrame()
+
+        for count, OA_area in enumerate(AreaOA_list, 1):
+            df_inactive_all_each_area = process_OA_area(
+                OA_area, df_economic_activity, df_composition, gender,
+                age_range, inactive_conversor)
+
+            # Append the dataframe into the dict
+            dict_key = f"df_{gender}_{age_range[0]}_{age_range[1]}_inactive_all_each_area"
+            persons_inactive[dict_key] = df_inactive_all_each_area
+
+        # Concatenate all persons "inactive" in one dataframe
+        df_inactive_all = pd.concat(
+            persons_inactive.values(), axis=0, ignore_index=True)
+        print('Number of people selected INACTIVE by age range and gender: ')
+        print(len(df_inactive_all.index))
+
+        # Calculate the TOTAL number of people with the same sex and
+        # range of age:
+        total = len(df_composition.loc[
+                        (df_composition['Sex'] == gender)
+                        & (df_composition['Age'] >= age_range[0])
+                        & (df_composition['Age'] <= age_range[1])
+                        ])
+        print('Total number of people age range and gender: ', total)
+
+        # Calculate the % of people inactive with the same sex and range
+        # of age:
+        total_inactive_percentage = len(df_inactive_all) / total * 100
+
+        # Compare the results against the ones given in table Regional
+        # labour market statistics:HI01 Headline indicators for the
+        # North East related to year 2019. If differences obtained
+        # against data given is within 1%, then it is Ok
+        if (((inactive_rate_2019 - 1) <= total_inactive_percentage)
+                & ((inactive_rate_2019 + 1) >= total_inactive_percentage)):
+            print('The value is within the tolerance of 1%')
+            print('Value obtained: ', total_inactive_percentage)
+            print('Continuing with the other gender or age range')
+            break
+
+        # If the difference is greater than a 1% (+/-) then a new
+        # iteration should be done updating the parameter that transform
+        # the employment rate from 2011 to 2019
+        else:
+            print('The % needs to be adjusted in another iteration')
+            print('Value obtained: ', total_inactive_percentage)
+            # If the difference is negative, then a POSITIVE increment
+            # has to be added
+            if (total_inactive_percentage - inactive_rate_2019 - 1) < 0:
+                # Update the value transform data from 2011 to 2019
+                # (increase the value):
+                inactive_conversor += 0.025
+            # If the difference is positive, then a NEGATIVE increment
+            # has to be added
+            else:
+                # Update the value transform data from 2011 to 2019 (reduce
+                # the value):
+                inactive_conversor -= 0.025
+
+            print('NEW CONVERSOR Value is: ', inactive_conversor)
+            continue
+
+
+def inactive_processing(df_composition, df_economic_activity, genders,
+                        age_range_list, AreaOA_list):
+
+    df_composition = generate_inactive_people(df_composition)
+    iteration_counter = 0
+
+    # TODO: Inactive rates (9:39-76) not called so have not been included
+
+    for gender in genders:
+        for age_range in age_range_list:
+            print('Processing: ', gender, str(age_range[0]), str(age_range[1]))
+
+            # TODO: Instead of using 'persons_inactive_list.clear()' I've
+            #  included persons_inactive list here to reset with each new
+            #  iteration. Does this break anything globally? Also changed to
+            #  dict.
+            persons_inactive = {}
+
+            total_inactive_percentage = 0
+
+            # TODO: What does this need to return? Iteration counter at least
+            inactive_iterator(iteration_counter, 0,
+                      AreaOA_list, df_economic_activity, df_composition, gender,
+                      age_range, persons_inactive)
+
+    # TODO: How should results be checked?
+    print('Inactive processing complete. Check the results.')
+
+
+def main(composition_path=os.getenv("composition_path"),
+         households_path=os.getenv("households_cleaned_path"),
+         economic_activity_path=os.getenv("households_cleaned_path")):
+
+    df_composition = pd.read_csv(composition_path, index_col=None, header=0)
+    df_households = pd.read_csv(households_path, index_col=None, header=0)
+    df_economic_activity = pd.read_csv(economic_activity_path, index_col=None,
+                                       header=0)
+
+    # Create a new empty column for the Economic_activity (empty string)
+    df_composition["Economic_activity"] = ""
+
+    # PARAMS TODO: Enable input config for params and make documentation
+    genders = {"male": 1, "female": 2}
+
+    # List containing the range of ages
+    # This values come from Regional labour market statistics: HI01 Headline indicators for the North East
+    # link: https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/headlinelabourforcesurveyindicatorsforthenortheasthi01
+    age_range_list = [(16, 24), (25, 34), (35, 49), (50, 64), (65, 120)]
+
+    ## LIST OF OA_AREAS that has been generated before
+    # Create a list with all Households unique ID values
+    AreaOA_list = list(set(df_households['Area_OA'].tolist()))
+
+    inactive_processing(df_composition, df_economic_activity, genders,
+                        age_range_list, AreaOA_list)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
